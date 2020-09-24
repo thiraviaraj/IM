91e474c8394c0c8d12a3bda8db39a770f3e6c68b
thiraviaraj <thiraviaraj>
2020-09-24 12:14:37 +0530

    ghPages Redirection

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const routes = [
 const fakeAuth = { isAuthenticated: true };
 function App(props) {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL + '/'}>
       <div className="App">
         <CssBaseline></CssBaseline>
         <Header></Header>
