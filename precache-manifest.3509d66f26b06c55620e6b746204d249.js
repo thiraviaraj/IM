97f3b93a64a0556263596fe653510b45a83b0766
thiraviaraj <thiraviaraj>
2020-09-24 10:49:58 +0530

    Updates

---
+++ precache-manifest.3509d66f26b06c55620e6b746204d249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19a4e8d852b48e9d9290d2ea67cbd40",
+    "url": "/IM/index.html"
+  },
+  {
+    "revision": "7e8a5a0796a7c4fbb90a",
+    "url": "/IM/static/css/main.b60f2f07.chunk.css"
+  },
+  {
+    "revision": "9cc8dc97b83aee5275ca",
+    "url": "/IM/static/js/2.202e4171.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/IM/static/js/2.202e4171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e8a5a0796a7c4fbb90a",
+    "url": "/IM/static/js/main.d48282d9.chunk.js"
+  },
+  {
+    "revision": "75b2e9a14130e0f38719",
+    "url": "/IM/static/js/runtime-main.de4f03ff.js"
+  }
+]);
